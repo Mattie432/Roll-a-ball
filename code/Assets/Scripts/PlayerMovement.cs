bc5a5b7a6ddb2136b147cc95110c54488d4c029c
Mattie432 <mattie432@icloud.com>
2015-02-02 16:49:25 +0000

    Tutorial 1 & 2 completed
    Game setup and ball movement in palce.

---
+++ code/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	public float speed;
+
+	// Use this for initialization, this runs when the script is loaded
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	//Called before performing any physics calculations. This is the one we will be using.
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rigidbody.AddForce (movement * speed * Time.deltaTime);
+	}
+}
